a606d9b636e07aceafbd9ae8d7fa18330b825e76
Kaioru <keifuurawrs@gmail.com>
2017-10-02 14:56:20 +0800

    Remove ability to index, update, show and destroy reports

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Report;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class ReportController extends Resource
 {
@@ -27,5 +28,25 @@ class ReportController extends Resource
     protected function model(): Model
     {
         return new Report();
+    }
+
+    public function index()
+    {
+        $this->response->errorForbidden();
+    }
+
+    public function beforeShow(Request $request, Model $model)
+    {
+        $this->response->errorForbidden();
+    }
+
+    public function beforeUpdate(Request $request, Model $model)
+    {
+        $this->response->errorForbidden();
+    }
+
+    public function beforeDestroy(Request $request, Model $model)
+    {
+        $this->response->errorForbidden();
     }
 }
